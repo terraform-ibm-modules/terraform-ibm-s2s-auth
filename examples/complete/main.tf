6a50afb5090d7bd4bcb797c1d27478e055e88a91
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-01-06 08:07:59 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v7.1.4 (#58)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 # Create COS instance
 module "cos_instance" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "7.1.3"
+  version                = "7.1.4"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
