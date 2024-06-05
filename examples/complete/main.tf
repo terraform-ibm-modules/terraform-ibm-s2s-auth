86ee800be62b6ffbeb9e9c690e8d0ac18ccc41de
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-05 11:39:31 +0100

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v8 (#91)
    * chore(deps): update terraform terraform-ibm-modules/cos/ibm to v8
    
    * SKIP UPGRADE TEST
    
    ---------
    
    Co-authored-by: Conall Ó Cofaigh <ocofaigh@ie.ibm.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 # Create COS instance
 module "cos_instance" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "7.5.3"
+  version                = "8.3.0"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
