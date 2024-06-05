b02e954e254762d12f64b1b0fda3162ffed7b26b
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2024-06-05 06:20:52 -0400

    fix(deps): update CBR module to `1.22.1` (#112)

--- version.tf
+++ version.tf
@@ -5,7 +5,7 @@ terraform {
     ibm = {
       source = "IBM-Cloud/ibm"
       # Use "greater than or equal to" range in modules
-      version = ">= 1.56.1"
+      version = ">= 1.62.0, <2.0.0"
     }
   }
 }
