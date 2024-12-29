eca85743ab1f70c411403f3a91131f75ccc82538
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-12-29 14:25:32 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v8.16.2

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 # Create COS instance
 module "cos_instance" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.16.1"
+  version                = "8.16.2"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
