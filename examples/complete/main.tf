62b49a20cd61eaf8c2d21e73456f9c825ebc8954
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-06-14 15:24:20 +0000

    fix(deps): update terraform dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,7 +8,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.2.0"
+  version = "1.2.1"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -17,7 +17,7 @@ module "resource_group" {
 # Create COS instance
 module "cos_instance" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "9.0.3"
+  version                = "9.0.6"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   retention_enabled      = false
