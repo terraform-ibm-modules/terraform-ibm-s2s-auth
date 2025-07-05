129725308148676f868d7db3eb7b8863ccbba2b9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-05 23:27:09 +0000

    fix(deps): update terraform dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,7 +8,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.2.0"
+  version = "1.2.1"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -28,7 +28,7 @@ module "cos_instance" {
 # Create Key Protect instance
 module "key_protect_instance" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.10.2"
+  version           = "2.10.4"
   key_protect_name  = "${var.prefix}-key-protect"
   resource_group_id = module.resource_group.resource_group_id
   plan              = "tiered-pricing"
