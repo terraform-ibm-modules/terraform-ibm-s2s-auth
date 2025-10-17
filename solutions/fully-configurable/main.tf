208b5791e4f5f14125cc1156dfb339301473cf3f
dishankkalra23 <dishankalra@gmail.com>
2025-10-17 19:13:16 +0530

    update: var.prefix

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -1,6 +1,10 @@
+locals {
+  prefix = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}-" : "" : ""
+}
+
 module "service_auth_policy" {
   source      = "../.."
-  prefix      = var.prefix
+  prefix      = local.prefix
   service_map = var.service_map
   enable_cbr  = false
 }
--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -11,7 +11,7 @@ variable "ibmcloud_api_key" {
 variable "prefix" {
   type        = string
   nullable    = true
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-cbr. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
+  description = "The prefix to add to all resources that this solution creates (e.g `prod`, `test`, `dev`). To skip using a prefix, set this value to `null` or an empty string. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
 
   validation {
     # - null and empty string is allowed
@@ -30,7 +30,7 @@ variable "prefix" {
 
   validation {
     # must not exceed 16 characters in length
-    condition     = length(var.prefix) <= 16
+    condition     = var.prefix == null || var.prefix == "" ? true : length(var.prefix) <= 16
     error_message = "Prefix must not exceed 16 characters."
   }
 }
