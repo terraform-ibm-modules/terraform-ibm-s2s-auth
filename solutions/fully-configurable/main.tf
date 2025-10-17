a0c209699a9f5cde5393253b67e652cf4bef6a17
dishankkalra23 <dishankalra@gmail.com>
2025-10-17 21:55:59 +0530

    fix: resolve review comments

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -1,10 +1,6 @@
-locals {
-  prefix = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}-" : "" : ""
-}
-
 module "service_auth_policy" {
   source      = "../.."
-  prefix      = local.prefix
+  prefix      = null
   service_map = var.service_map
   enable_cbr  = false
 }
