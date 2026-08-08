dd950c5748d7d67d86eb09aa27e8bd388ebe3c8c
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-08-08 12:40:46 +0000

    fix(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,7 +21,7 @@ module "resource_group" {
 # Create COS instance
 module "cos_instance" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.5"
+  version                = "10.17.6"
   cos_instance_name      = "${var.prefix}-cos"
   kms_encryption_enabled = false
   resource_group_id      = module.resource_group.resource_group_id
@@ -31,7 +31,7 @@ module "cos_instance" {
 # Create Key Protect instance
 module "key_protect_instance" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "2.13.5"
+  version           = "2.13.6"
   key_protect_name  = "${var.prefix}-key-protect"
   resource_group_id = module.resource_group.resource_group_id
   plan              = "tiered-pricing"
